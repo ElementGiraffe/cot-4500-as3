3e5615c8949c30c5a1b1ebc448cd9051cad68208
ElementGiraffe <56352992+ElementGiraffe@users.noreply.github.com>
2023-04-02 18:04:00 -0400

    Parts 4, 5, 6

--- src/main/assignment_3.py
+++ src/main/assignment_3.py
@@ -91,7 +91,82 @@ def gaussian(A):
 
     return x
 
+def LU_factorization(A):
+    n = A.shape[0]
+    L = np.zeros((n, n))
+    U = np.zeros((n, n))
 
+    L[0][0] = 1
+    U[0][0] = A[0][0]
+
+    if U[0][0] == 0:
+        return None, None
+
+    for j in range(1, n):
+        U[0][j] = A[0][j]/L[0][0]
+        L[j][0] = A[j][0]/U[0][0]
+
+    for i in range(1, n-1):
+        L[i][i] = 1
+        U[i][i] = A[i][i]
+        for k in range(0, i):
+            U[i][i] -= L[i][k] * U[k][i]
+
+        if U[i][i] == 0:
+            return None, None
+
+        for j in range(i+1, n):
+            U[i][j] = A[i][j]
+            L[j][i] = A[j][i]
+
+            for k in range(0, i):
+                U[i][j] -= L[i][k] * U[k][j]
+                L[j][i] -= L[j][k] * U[k][i]
+
+            U[i][j] /= L[i][i]
+            L[j][i] /= U[i][i]
+
+    L[n-1][n-1] = 1
+    U[n-1][n-1] = A[n-1][n-1]
+    for k in range(0, n-1):
+        U[n-1][n-1] -= L[n-1][k] * U[k][n-1]
+
+    return L, U
+
+def is_diag_dom(A):
+    n = A.shape[0]
+
+    for i in range(n):
+        sum = 0
+        for j in range(n):
+            if i == j:
+                continue
+            sum += abs(A[i][j])
+        if abs(A[i][i]) < sum:
+            return False
+
+    return True
+
+
+def is_pos_def(A):
+    n = A.shape[0]
+
+    # checking that A is symmetric
+    for i in range(n):
+        for j in range(i+1, n):
+            if A[i][j] != A[j][i]:
+                return False
+
+    for i in range(n):
+        A_sub = A[:i+1, :i+1]
+
+        # print(i)
+        # print(A_sub)
+
+        if np.linalg.det(A_sub) <= 0:
+            return False
+
+    return True
 
 
 if __name__ == "__main__":
@@ -116,6 +191,47 @@ if __name__ == "__main__":
     sols = gaussian(A)
     print(sols)
 
+    print("")
 
+    #PART FOUR
 
+    A = np.array([[1, 1, 0, 3],
+                  [2, 1, -1, 1],
+                  [3, -1, -1, 2],
+                  [-1, 2, 3, -1]], dtype=np.double)
 
+    print("%.5f" % np.linalg.det(A))
+
+    L, U = LU_factorization(A)
+
+    print("")
+
+    print(L)
+
+    print("")
+
+    print(U)
+
+    #print(np.matmul(L, U))
+
+    print("")
+
+    # PART FIVE
+
+    A = np.array([[9, 0, 5, 2, 1],
+                  [3, 9, 1, 2, 1],
+                  [0, 1, 7, 2, 3],
+                  [4, 2, 3, 12, 2],
+                  [3, 2, 4, 0, 8]])
+
+    print(is_diag_dom(A))
+
+    print("")
+
+    # PART SIX
+
+    A = np.array([[2, 2, 1],
+                  [2, 3, 0],
+                  [1, 0, 2]])
+
+    print(is_pos_def(A))
