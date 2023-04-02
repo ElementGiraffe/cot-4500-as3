ca1c3e55ec4881a016959f72aa93fd06246df57c
ElementGiraffe <56352992+ElementGiraffe@users.noreply.github.com>
2023-04-02 16:41:07 -0400

    Parts 1, 2, 3 complete

---
+++ src/main/assignment_3.py
@@ -0,0 +1,121 @@
+import numpy as np
+
+# takes in function f(t, y), the range [a, b], the number of iterations N, and f(a) = alpha
+def eulers_method(func, a, b, N, alpha):
+
+    ans = []
+
+    h = (b-a)/N
+    t = a
+    w = alpha
+
+    ans.append(eval_fty(func, t, w))
+
+    for i in range(1, N+1):
+        w += h*eval_fty(func, t, w)
+        t += h
+        ans.append(eval_fty(func, t, w))
+
+    #print(ans)
+    return w
+
+def eval_fty(f, t, y):
+    # print(f)
+    # print("t: " + str(t))
+    # print("y: " + str(y))
+    # print(eval(f))
+    return eval(f)
+
+# this is of order four
+def runge_kutta(func, a, b, N, alpha):
+    ans = []
+
+    h = (b-a)/N
+    t = a
+    w = alpha
+
+    ans.append(eval_fty(func, t, w))
+
+    for i in range(1, N+1):
+        K1 = h*eval_fty(func, t, w)
+        K2 = h*eval_fty(func, t + h/2, w + K1/2)
+        K3 = h*eval_fty(func, t + h/2, w + K2/2)
+        K4 = h*eval_fty(func, t + h, w + K3)
+
+        w += (K1 + 2*K2 + 2*K3 + K4)/6
+        t += h
+
+        ans.append(eval_fty(func, t, w))
+
+    # print(ans)
+
+    return w
+
+# performs gaussian elimination and returns an array of the values of x
+def gaussian(A):
+    n = A.shape[0]
+
+    for i in range(n-1):
+        p = -1
+        for p_test in range(i, n):
+            if A[p_test][i] != 0:
+                p = p_test
+                break
+
+        if p == -1:
+            # no solution was found
+            return None
+
+        if p != i:
+            # swap rows
+            A[[p, i]] = A[[i, p]]
+
+        for j in range(i+1, n):
+            m = A[j][i]/A[i][i]
+            A[j] = A[j] - m*A[i]
+
+    if A[n-1][n-1] == 0:
+        # infinite solutions
+        return None
+
+    # print(A)
+    x = np.zeros(n)
+
+    x[n-1] = A[n-1][n]/A[n-1][n-1]
+
+    for i in range(n-2, -1, -1):
+        x[i] = A[i][n]
+        for j in range(i+1, n):
+            x[i] -= A[i][j]*x[j]
+        x[i] /= A[i][i]
+
+    return x
+
+
+
+
+if __name__ == "__main__":
+    np.set_printoptions(precision=7, suppress=True, linewidth=100)
+
+    # PART ONE
+    euler_ans = eulers_method("t - y**2", 0, 2, 10, 1)
+    print("%.5f" % euler_ans)
+
+    print("")
+
+    # PART TWO
+    runge_kutta_ans = runge_kutta("t - y**2", 0, 2, 10, 1)
+    print("%.5f" % runge_kutta_ans)
+
+    print("")
+    # PART THREE
+    A = np.array([[2, -1, 1, 6],
+                  [1, 3, 1, 0],
+                  [-1, 5, 4, -3]], dtype=np.double)
+
+    sols = gaussian(A)
+    print(sols)
+
+
+
+
